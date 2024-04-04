0d3d79895a79aa4f55e2b36f216b1f7506f9f6f7
ppparkta <pppppark2@gmail.com>
2024-04-04 17:08:51 +0900

    design/#1: 발작일지 상세 화면 레이아웃 개발(이미지 미추가)

--- app/src/main/java/com/epi/epilog/MainActivity.kt
+++ app/src/main/java/com/epi/epilog/MainActivity.kt
@@ -15,7 +15,7 @@ import com.epi.epilog.ui.theme.EpilogTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_seizure_edit)
     }
 }
 
