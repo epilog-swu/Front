c87fd4a0e7b11cb67909ed95761d9160c7bf84d0
ppparkta <pppppark2@gmail.com>
2024-04-06 01:24:32 +0900

    design/#1: 발작일지 작성 7번 레이아웃 개발

--- app/src/main/java/com/epi/epilog/MainActivity.kt
+++ app/src/main/java/com/epi/epilog/MainActivity.kt
@@ -12,31 +12,32 @@ import com.epi.epilog.databinding.ActivityModeSelectBinding
 import com.epi.epilog.databinding.ActivitySeizureDetailBinding
 import com.epi.epilog.databinding.ActivitySeizureEditBinding
 import com.epi.epilog.databinding.SeizureEdit2Binding
+import com.epi.epilog.databinding.SeizureEdit7Binding
 import com.epi.epilog.databinding.SignUp1Binding
 import com.epi.epilog.databinding.SignUp2Binding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivitySeizureDetailBinding.inflate(layoutInflater)
+        val binding = ActivitySeizureEditBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-//        val fragmentManager: FragmentManager = supportFragmentManager
-//        val transaction: FragmentTransaction = fragmentManager.beginTransaction()
-//        val seizureFragment = SeizureFragment()
-//        transaction.add(R.id.seizure_edit_fragment, seizureFragment)
-//        transaction.commit()
+        val fragmentManager: FragmentManager = supportFragmentManager
+        val transaction: FragmentTransaction = fragmentManager.beginTransaction()
+        val seizureFragment = SeizureFragment()
+        transaction.add(R.id.seizure_edit_fragment, seizureFragment)
+        transaction.commit()
     }
 }
 
 class SeizureFragment: Fragment() {
-    lateinit var binding: SeizureEdit2Binding
+    lateinit var binding: SeizureEdit7Binding
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = SeizureEdit2Binding.inflate(inflater, container, false)
+        binding = SeizureEdit7Binding.inflate(inflater, container, false)
         return binding.root
     }
 }
