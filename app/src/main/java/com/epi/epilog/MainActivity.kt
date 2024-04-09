382fcddbe0a2bebde2ae7c9cafab5bab46142a20
xlebin <ssysjy0302@gmail.com>
2024-04-09 23:40:15 +0900

    design/#5 : 0409 회의 중간 merge

--- app/src/main/java/com/epi/epilog/MainActivity.kt
+++ app/src/main/java/com/epi/epilog/MainActivity.kt
@@ -1,21 +1,33 @@
 package com.epi.epilog
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material3.ModalBottomSheet
+import androidx.fragment.app.DialogFragment
 import com.epi.epilog.databinding.ActivitySeizureDetailBinding
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //val binding = ActivitySeizureDetailBinding.inflate(layoutInflater)
-        setContentView(R.layout.main_calendar_1)
+        setContentView(R.layout.test)
 
+//        val bottomSheetView = layoutInflater.inflate(R.layout.test, null)
+//        val bottomSheetDialog = BottomSheetDialog(this)
+//        bottomSheetDialog.setContentView(bottomSheetView)
+//
+//        findViewById<Button>(R.id.test).setOnClickListener {
+//            bottomSheetDialog.show()
+//        }
 //        val fragmentManager: FragmentManager = supportFragmentManager
 //        val transaction: FragmentTransaction = fragmentManager.beginTransaction()
 //        val seizureFragment = SeizureFragment()
 //        transaction.add(R.id.seizure_edit_fragment, seizureFragment)
 //        transaction.commit()
     }
+
 }
 //
 //class SeizureFragment: Fragment() {
