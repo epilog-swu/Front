7dd071661b727d2dad2856aea6fe326de8e77a47
gusdk45 <gusdk1418@naver.com>
2024-04-09 22:58:17 +0900

    design/#8 : 회원가입 페이지 디자인 완료

--- app/src/main/java/com/epi/epilog/MainActivity.kt
+++ app/src/main/java/com/epi/epilog/MainActivity.kt
@@ -1,26 +1,30 @@
 package com.epi.epilog
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.startActivity
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentTransaction
-import com.epi.epilog.databinding.ActivityModeSelectBinding
-import com.epi.epilog.databinding.ActivitySeizureDetailBinding
-import com.epi.epilog.databinding.ActivitySeizureEditBinding
 import com.epi.epilog.databinding.SeizureEdit2Binding
 import com.epi.epilog.databinding.SignUp1Binding
 import com.epi.epilog.databinding.SignUp2Binding
+import com.epi.epilog.databinding.SignUp3Binding
+import com.epi.epilog.databinding.SignUp4Binding
+import com.epi.epilog.databinding.SignUp5Binding
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivitySeizureDetailBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+//        val binding = ActivitySeizureDetailBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
 
+        val intent = Intent(this, SignUp5Activity::class.java)
+        startActivity(intent)
 //        val fragmentManager: FragmentManager = supportFragmentManager
 //        val transaction: FragmentTransaction = fragmentManager.beginTransaction()
 //        val seizureFragment = SeizureFragment()
@@ -40,3 +44,52 @@ class SeizureFragment: Fragment() {
         return binding.root
     }
 }
+
+class SignUp1Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = SignUp1Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+}
+class SignUp2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = SignUp2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+}
+
+class SignUp3Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = SignUp3Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+}
+
+class SignUp4Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = SignUp4Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+}
+class SignUp5Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = SignUp5Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+}
+
