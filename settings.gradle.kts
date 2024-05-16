10952e5c5d5a56a0ef4d7a2aecdea174d29dd13a
xlebin <ssysjy0302@gmail.com>
2024-05-16 16:38:45 +0900

    Feat/#18 워치 메인 레이아웃 작성

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,13 +11,17 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://jitpack.io") }
-    }}
+        maven(url = uri("https://jitpack.io"))
+        jcenter()  // 이전에 jcenter() 리포지터리가 포함되어 있었으나 이제는 사용하지 않는 것을 권장합니다.
+        // 필요한 경우만 포함하시고, 가능하다면 대체 리포지터리를 사용하세요.
+    }
+}
 
 rootProject.name = "epilog"
 include(":app")
--- wear/build.gradle.kts
+++ wear/build.gradle.kts
@@ -29,6 +29,10 @@ android {
         }
     }
     compileOptions {
+
+        //calendar 라이브러리 사용 위해 임포트
+        isCoreLibraryDesugaringEnabled = true
+
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
@@ -50,6 +54,17 @@ android {
 
 dependencies {
 
+    //calendar 라이브러리 사용하려고 추가
+    coreLibraryDesugaring ("com.android.tools:desugar_jdk_libs:2.0.4")
+    implementation("com.kizitonwose.calendar:compose:2.5.1") // Compose
+    implementation("com.kizitonwose.calendar:view:2.5.1") // View
+    //implementation ("com.github.kizitonwose:CalendarView:2.5.1")
+
+    //그래프 라이브러리 사용하려고 추가
+    implementation ("com.github.PhilJay:MPAndroidChart:v3.1.0")
+
+
+
     implementation(libs.play.services.wearable)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
@@ -62,6 +77,7 @@ dependencies {
     implementation("androidx.recyclerview:recyclerview:1.2.1")
     implementation("com.google.android.material:material:1.4.0")
     implementation(libs.androidx.wear)
+    implementation(libs.constraintlayout)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
