d449e750a8d2f4de14f0a86f12694cf8ec1ce3fe
xlebin <ssysjy0302@gmail.com>
2024-05-22 00:36:56 +0900

    Feat/#27 메인 그래프 UI 수정

--- wear/src/main/java/com/epi/epilog/presentation/MainActivity.kt
+++ wear/src/main/java/com/epi/epilog/presentation/MainActivity.kt
@@ -14,7 +14,6 @@ import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
 import com.github.mikephil.charting.formatter.ValueFormatter
 
-
 class MainActivity : ComponentActivity() {
 
     private lateinit var binding: ActivityMainBinding
@@ -25,7 +24,6 @@ class MainActivity : ComponentActivity() {
 
     private var todayDataCounts: Int = 0
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(android.R.style.Theme_DeviceDefault)
@@ -34,14 +32,12 @@ class MainActivity : ComponentActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
         // chart 초기화
         chart = binding.graphBloodSugarChart
 
         // 그래프 데이터 설정 및 차트 초기화
         initChartData()
         initChart()
-
 
         //버튼 클릭 리스너 구현
         binding.btnBloodSugarRecord.setOnClickListener {
@@ -53,8 +49,7 @@ class MainActivity : ComponentActivity() {
 
         binding.btnCheckMedicine.setOnClickListener {
             val intent = Intent(
-                this,
-                MedicineActivity::class.java
+                this, MedicineActivity::class.java
             )
             startActivity(intent)
         }
@@ -66,6 +61,12 @@ class MainActivity : ComponentActivity() {
             startActivity(intent)
         }
 
+        binding.btnCheckGraph.setOnClickListener {
+            val intent = Intent(
+                this, GraphActivity::class.java
+            )
+            startActivity(intent)
+        }
     }
 
     // 차트 데이터 초기화 메서드
@@ -87,9 +88,9 @@ class MainActivity : ComponentActivity() {
         set.setDrawValues(false)
 
         //색상
-        set.color = Color.parseColor("#FFFFFF")// Line의 색상을 설정 //흰색
+        set.color = Color.parseColor("#A798E5") // Line의 색상을 설정
         val circleColor = Color.parseColor("#8377D8") //메인보라
-        set.setCircleColors(circleColor) //데이터 셋의 색상을 설정 //메인 보라
+        set.setCircleColors(circleColor) //데이터 셋의 색상을 설정
 
         //강조 표시
         set.highLightColor = Color.TRANSPARENT
@@ -106,89 +107,74 @@ class MainActivity : ComponentActivity() {
             invalidate() // 차트를 다시 그립니다.
 
             // 좌우 스크롤 및 줌 가능하게 설정
-            setDragEnabled(true)
-            setScaleEnabled(true)
-            setPinchZoom(true)
-            setVisibleXRangeMaximum(3f) // 한 번에 보이는 최대 x축 범위 설정
+            //setDragEnabled(true)
+            //setScaleEnabled(true)
+            //setPinchZoom(true)
+            //setVisibleXRangeMaximum(7f) // 한 번에 보이는 최대 x축 범위 설정
             moveViewToX(0f) // 초기 위치 설정
         }
 
-
         // Description 설정 (제목)
-        val description = Description().apply {
-            text = "오늘의 혈당 기록 개수 : $todayDataCounts"
-            textSize = 7f
-            textColor = Color.BLACK
-        }
-        chart.description = description
+//        val description = Description().apply {
+//            text = "오늘의 혈당 기록 개수 : $todayDataCounts"
+//            textSize = 7f
+//            textColor = Color.BLACK
+//        }
+//        chart.description = description
 
-        // ViewTreeObserver를 사용하여 레이아웃이 완료된 후에 위치 설정
-        chart.viewTreeObserver.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
-            override fun onGlobalLayout() {
-                description.setPosition(chart.width.toFloat() - 60f, 20f)
-                chart.description = description
-                chart.viewTreeObserver.removeOnGlobalLayoutListener(this)
+            // ViewTreeObserver를 사용하여 레이아웃이 완료된 후에 위치 설정
+            chart.viewTreeObserver.addOnGlobalLayoutListener(object :
+                ViewTreeObserver.OnGlobalLayoutListener {
+                override fun onGlobalLayout() {
+                    chart.viewTreeObserver.removeOnGlobalLayoutListener(this)
+                }
+            })
+
+            // x축 설정
+            val xAxis = chart.xAxis
+            xAxis.apply {
+                setDrawLabels(false) // 라벨 표시 안함
+                axisMaximum = 1200f
+                axisMinimum = -240f
+                granularity = 240f // X축 간격 설정
+                textColor = Color.BLACK
+                textSize = 0.05f // 텍스트 크기 설정
+                position = XAxis.XAxisPosition.BOTTOM // x축 라벨 위치
+                setDrawGridLines(false) // Y축 평행 격자선 제거
+                setDrawAxisLine(true) // Axis-line 표시
             }
-        })
 
-
-        //x, y축 설정
-
-        val xAxis = chart.xAxis
-        xAxis.apply {
-            setDrawLabels(true)
-            axisMaximum = 1680f
-            axisMinimum = -240f
-            granularity = 240f // X축 간격 설정
-            valueFormatter = object : ValueFormatter() {
-                private val labels = arrayOf("공복", "아침1", "아침2", "점심1", "점심2", "저녁1", "저녁2", "자기 전")
-                override fun getFormattedValue(value: Float): String {
-                    val index = ((value + 240) / 240).toInt()
-                    return if (index >= 0 && index < labels.size) labels[index] else ""
+            val yAxis = chart.axisLeft
+            yAxis.apply {
+                axisMaximum = 350f // 범위를 약간 더 크게 설정
+                axisMinimum = 0f
+                granularity = 50f // Y축 간격 설정
+                setLabelCount(8, true)
+                textColor = Color.BLACK
+                textSize = 0.3f // 텍스트 크기 설정
+                valueFormatter = object : ValueFormatter() {
+                    override fun getFormattedValue(value: Float): String {
+                        return value.toInt().toString()
+                    }
                 }
             }
-            textColor = Color.BLACK
-            textSize = 0.05f // 텍스트 크기 설정
-            position = XAxis.XAxisPosition.BOTTOM // x축 라벨 위치
-            setDrawGridLines(false) // Y축 평행 격자선 제거
-            setDrawAxisLine(true) // Axis-line 표시
-        }
 
-
-        val yAxis = chart.axisLeft
-        yAxis.apply {
-            axisMaximum = 350f // 범위를 약간 더 크게 설정
-            axisMinimum = 0f
-            granularity = 50f // Y축 간격 설정
-            setLabelCount(8, true)
-            textColor = Color.BLACK
-            textSize = 0.3f // 텍스트 크기 설정
-            valueFormatter = object : ValueFormatter() {
-                override fun getFormattedValue(value: Float): String {
-                    return value.toInt().toString()
-                }
+            val yRAxis = chart.axisRight
+            yRAxis.apply {
+                setDrawLabels(false)
+                setDrawAxisLine(false)
+                setDrawGridLines(false)
             }
+
+
+
+            chart.description.isEnabled = false
+            chart.data = lineData // 데이터 설정
+
+            // Legend 설정 (그래프 제목의 크기를 줄임)
+            val legend = chart.legend
+            legend.isEnabled = false // 제목을 숨김
+
+            chart.invalidate() // 다시 그리기
         }
-
-
-        val yRAxis = chart.axisRight
-        yRAxis.apply {
-            setDrawLabels(false)
-            setDrawAxisLine(false)
-            setDrawGridLines(false)
-        }
-
-        chart!!.description.isEnabled = true
-        chart!!.data = lineData //데이터 설정
-
-
-        // Legend 설정 (그래프 제목의 크기를 줄임)
-        val legend = chart.legend
-        legend.isEnabled = false // 제목을 숨김
-        //legend.setTextSize(0.5f);
-
-        chart!!.description.isEnabled = true
-        chart!!.invalidate() //다시 그리기
-
     }
-}
