36e62b4694e5528a08867a0583c11b107e627aac
gusdk45 <gusdk1418@naver.com>
2024-06-01 14:43:44 +0900

    feat/#29 : 낙상감지 포그라운드 서비스로

--- wear/src/main/java/com/epi/epilog/presentation/MainActivity.kt
+++ wear/src/main/java/com/epi/epilog/presentation/MainActivity.kt
@@ -28,7 +28,7 @@ import retrofit2.Response
 import com.kizitonwose.calendar.view.WeekCalendarView
 import java.time.LocalDate
 
-class MainActivity : ComponentActivity(), SensorEventListener {
+class MainActivity : ComponentActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var chart: LineChart
@@ -60,9 +60,6 @@ class MainActivity : ComponentActivity(), SensorEventListener {
         calendarInitializer = CalendarInitializer(this, weekCalendarView, this::onDateSelected) // 변경된 부분: 초기화
         calendarInitializer.initWeekCalendarView()
 
-        //로그인 시 토큰 발급
-        initializeRetrofit()
-        postData()
 
         // ChartInitializer 사용
         ChartInitializer(chart).initChart()
@@ -70,10 +67,6 @@ class MainActivity : ComponentActivity(), SensorEventListener {
         // 버튼 클릭 리스너 설정
         setButtonListeners()
 
-        // Initialize sensors
-        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
-        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_FASTEST)
     }
 
     private fun setButtonListeners() {
@@ -90,49 +83,7 @@ class MainActivity : ComponentActivity(), SensorEventListener {
         }
     }
 
-    private fun getTokenFromSession(): String? {
-        val sharedPreferences = getSharedPreferences("AppPrefs", Context.MODE_PRIVATE)
-        return sharedPreferences.getString("AuthToken", null)
-    }
 
-    private fun disableButtons() {
-        binding.btnBloodSugarRecord.isEnabled = false
-        binding.btnCheckMedicine.isEnabled = false
-        binding.btnCheckMeals.isEnabled = false
-    }
-
-    private fun enableButtons() {
-        binding.btnBloodSugarRecord.isEnabled = true
-        binding.btnCheckMedicine.isEnabled = true
-        binding.btnCheckMeals.isEnabled = true
-    }
-
-    private fun initChartData() {
-        bloodSugarChartData.add(Entry(0f, 50f))
-        bloodSugarChartData.add(Entry(240f, 70f))
-        bloodSugarChartData.add(Entry(480f, 120f))
-        bloodSugarChartData.add(Entry(720f, 90f))
-        bloodSugarChartData.add(Entry(960f, 250f))
-        bloodSugarChartData.add(Entry(1200f, 300f))
-        bloodSugarChartData.add(Entry(1440f, 30f))
-
-        todayDataCounts = 3
-
-        //데이터 이름 및 Line 설정
-        val set = LineDataSet(bloodSugarChartData, "Blood Sugar Levels")
-        set.setDrawValues(false)
-
-        //색상
-        set.color = Color.parseColor("#FFFFFF")
-        val circleColor = Color.parseColor("#8377D8")
-        set.setCircleColors(circleColor)
-
-        //강조 표시
-        set.highLightColor = Color.TRANSPARENT
-        set.mode = LineDataSet.Mode.LINEAR
-
-        lineData = LineData(set)
-    }
 
     private fun navigateToActivity(activityClass: Class<*>) {
         val intent = Intent(this, activityClass).apply {
@@ -146,104 +97,6 @@ class MainActivity : ComponentActivity(), SensorEventListener {
     private fun onDateSelected(date: LocalDate) {
     }
 
-    private fun initializeRetrofit() {
-        val gson: Gson = GsonBuilder()
-            .setLenient()
-            .create()
-        val retrofit = Retrofit.Builder()
-            .baseUrl("http://epilog-develop-env.eba-imw3vi3g.ap-northeast-2.elasticbeanstalk.com/")
-            .addConverterFactory(GsonConverterFactory.create(gson))
-            .build()
-        retrofitService = retrofit.create(RetrofitService::class.java)
-    }
 
-    private fun postData() {
-        val post = Data()
-        val call = retrofitService.postData(post)
-        call.enqueue(object : Callback<String> {
-            override fun onResponse(call: Call<String>, response: Response<String>) {
-                if (response.isSuccessful) {
-                    response.body()?.let {
-                        Log.d("BloodSugarTimeInput", "Server Response: $it")
-                        saveTokenToSession(it)
-                        runOnUiThread {
-                            setupButtonListeners()
-                        }
-                    }
-                } else {
-                    val errorBody = response.errorBody()?.string()
-                    Log.d("BloodSugarTimeInput", "Error Response: $errorBody")
-                    Log.d("BloodSugarTimeInput", "Response Code: ${response.code()}")
-                    runOnUiThread {
-                        disableButtons()
-                    }
-                }
-            }
 
-            override fun onFailure(call: Call<String>, t: Throwable) {
-                Log.d("BloodSugarTimeInput", "POST failed: ${t.message}")
-                runOnUiThread {
-                    disableButtons()
-                }
-            }
-        })
-    }
-
-    private fun setupButtonListeners() {
-        val token = getTokenFromSession()
-        if (token.isNullOrEmpty()) {
-            disableButtons()
-        } else {
-            enableButtons()
-        }
-    }
-
-    private fun saveTokenToSession(token: String) {
-        val sharedPreferences = getSharedPreferences("AppPrefs", Context.MODE_PRIVATE)
-        sharedPreferences.edit().putString("AuthToken", token).apply()
-    }
-
-    override fun onSensorChanged(event: SensorEvent?) {
-        event?.let {
-            val x = it.values[0]
-            val y = it.values[1]
-            val z = it.values[2]
-            sensorData.add(SensorData(x, y, z))
-
-            if (sensorData.size == 100) {
-                Log.d("SensorData", "Sending data: $sensorData")
-                postSensorData()
-                sensorData.clear()
-            }
-        }
-    }
-
-    private fun postSensorData() {
-        val token = getTokenFromSession()
-        if (token.isNullOrEmpty()) {
-            Log.d("BloodSugarTimeInput", "Auth token is missing.")
-            return
-        }
-
-        val call = retrofitService.postSensorData(sensorData, "Bearer $token")
-        call.enqueue(object : Callback<Boolean> {
-            override fun onResponse(call: Call<Boolean>, response: Response<Boolean>) {
-                if (response.isSuccessful) {
-                    val responseBody = response.body()
-                    Log.d("BloodSugarTimeInput", "Sensor Data Response: $responseBody")
-                } else {
-                    val errorBody = response.errorBody()?.string()
-                    Log.e("BloodSugarTimeInput", "Response was not successful: Code ${response.code()}, Error Body: $errorBody")
-                }
-            }
-
-            override fun onFailure(call: Call<Boolean>, t: Throwable) {
-                Log.e("BloodSugarTimeInput", "POST failed: ${t.message}")
-            }
-        })
-    }
-
-    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
-        // 필요한 경우 정확도 변경 처리
-    }
 }
